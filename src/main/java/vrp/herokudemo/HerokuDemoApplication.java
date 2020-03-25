66c860c37481a5a143e40aedaf2879a2d1eae85e
bondarchukk <kbandarchyk@gmail.com>
2020-03-25 12:37:48 +0300

    master: added profiles

--- src/main/java/vrp/herokudemo/HerokuDemoApplication.java
+++ src/main/java/vrp/herokudemo/HerokuDemoApplication.java
@@ -2,8 +2,10 @@ package vrp.herokudemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource({"classpath:/profile.properties", "classpath:/application.properties"})
 public class HerokuDemoApplication {
     
     public static void main( String [] args ){
--- src/main/java/vrp/herokudemo/config/dev/R2DBCDatabaseConfig.java
+++ src/main/java/vrp/herokudemo/config/dev/R2DBCDatabaseConfig.java
@@ -1,16 +1,15 @@
-package vrp.herokudemo.config;
+package vrp.herokudemo.config.dev;
 
 import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
 import io.r2dbc.postgresql.PostgresqlConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
+@Profile( "dev" )
 @Configuration
 @EnableR2dbcRepositories( basePackages  = "vrp.herokudemo.repository")
 public class R2DBCDatabaseConfig extends AbstractR2dbcConfiguration {
@@ -21,7 +20,7 @@ public class R2DBCDatabaseConfig extends AbstractR2dbcConfiguration {
     private final String username;
     private final String password;
     
-    /*public R2DBCDatabaseConfig( final @Value("${r2dbc.connection.host}") String host
+    public R2DBCDatabaseConfig( final @Value("${r2dbc.connection.host}") String host
                               , final @Value("${r2dbc.connection.port}")int port
                               , final @Value("${r2dbc.connection.database}")String database
                               , final @Value("${r2dbc.connection.username}")String username
@@ -31,17 +30,6 @@ public class R2DBCDatabaseConfig extends AbstractR2dbcConfiguration {
         this.database = database;
         this.username = username;
         this.password = password;
-    }*/
-    
-    public R2DBCDatabaseConfig( final @Value( "${database.url}" ) String dataBaseURLString ) throws URISyntaxException {
-        
-        final URI dataBaseURL = new URI( dataBaseURLString );
-        
-        this.host = dataBaseURL.getHost();
-        this.port = dataBaseURL.getPort();
-        this.database = dataBaseURL.getPath().substring( 1 );
-        this.username = dataBaseURL.getUserInfo().split(":")[0];
-        this.password = dataBaseURL.getUserInfo().split(":")[1];
     }
     
     @Bean
