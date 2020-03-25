8903e36b38cbcd9ef6b5af853279b5a2aff3723b
bondarchukk <kbandarchyk@gmail.com>
2020-03-25 18:14:55 +0300

    master: some fixes with migration script

--- src/main/resources/db/migration/V1_1__add_users_table.sql
+++ src/main/resources/db/migration/V1_1__add_users_table.sql
@@ -2,9 +2,9 @@ create table users (
   id bigserial not null,
   first_name varchar(256) not null,
   last_name varchar(256) not null,
-  login varchar(20) not null,
-  password varchar(20) not null,
+  login varchar(256) not null,
+  password varchar(256) not null,
   is_deleted boolean not null,
   primary key (id) );
 
-INSERT INTO users( first_name, last_name, login, password, id_deleted ) VALUES ( 'Иван', 'Иванов', 'admin', '$2y$12$z/tAJFs2P86OoQ5OpXJqG.OvjhwhMuyaK5HLZJkIaSZny8w18Jz16', false );
+INSERT INTO users( first_name, last_name, login, password, is_deleted ) VALUES ( 'Иван', 'Иванов', 'admin', '$2y$12$z/tAJFs2P86OoQ5OpXJqG.OvjhwhMuyaK5HLZJkIaSZny8w18Jz16', false );
