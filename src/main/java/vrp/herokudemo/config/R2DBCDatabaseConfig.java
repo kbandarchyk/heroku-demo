a7ecfe86750156f0661517a8629992d7c66700de
bondarchukk <kbandarchyk@gmail.com>
2020-03-25 11:36:10 +0300

    master: some fixes with database config

--- src/main/java/vrp/herokudemo/config/R2DBCDatabaseConfig.java
+++ src/main/java/vrp/herokudemo/config/R2DBCDatabaseConfig.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @Configuration
 @EnableR2dbcRepositories( basePackages  = "vrp.herokudemo.repository")
 public class R2DBCDatabaseConfig extends AbstractR2dbcConfiguration {
@@ -18,7 +21,7 @@ public class R2DBCDatabaseConfig extends AbstractR2dbcConfiguration {
     private final String username;
     private final String password;
     
-    public R2DBCDatabaseConfig( final @Value("${r2dbc.connection.host}") String host
+    /*public R2DBCDatabaseConfig( final @Value("${r2dbc.connection.host}") String host
                               , final @Value("${r2dbc.connection.port}")int port
                               , final @Value("${r2dbc.connection.database}")String database
                               , final @Value("${r2dbc.connection.username}")String username
@@ -28,6 +31,17 @@ public class R2DBCDatabaseConfig extends AbstractR2dbcConfiguration {
         this.database = database;
         this.username = username;
         this.password = password;
+    }*/
+    
+    public R2DBCDatabaseConfig( final @Value( "${database.url}" ) String dataBaseURLString ) throws URISyntaxException {
+        
+        final URI dataBaseURL = new URI( dataBaseURLString );
+        
+        this.host = dataBaseURL.getHost();
+        this.port = dataBaseURL.getPort();
+        this.database = dataBaseURL.getPath().substring( 1 );
+        this.username = dataBaseURL.getUserInfo().split(":")[0];
+        this.password = dataBaseURL.getUserInfo().split(":")[1];
     }
     
     @Bean
